3ad12dfd406be164c2f323b4a1e825dbc201c483
vikor <vikor@gmail.com>
2016-10-20 22:12:08 +0300

    chapter 1

--- src/main/scala/chapter1/Chapter1q3.scala
+++ src/main/scala/chapter1/Chapter1q3.scala
@@ -1,42 +1,20 @@
 
 object Chapter1q3 {
 
-  def urlEncode(str: String): String = {
-    //var buf = "".toSeq
-    val urlSeq: Seq[Char] = str
+  def urlEncode(url: String): String = {
     @annotation.tailrec
-    def go(cur: Seq[Char], acc: Seq[Char]): Seq[Char] = {
-      println(s"cur:${cur} acc:${acc}"); 
-      cur match {
-          case Nil                  => acc
-          case head :: tail if (head == ' ') => go(tail, acc)
-          case head :: tail                  => go(tail, acc :+ head)
-          case x => error(x.toString)
-      }
+    def go(cur: String, acc: String): String = cur match {
+       case ""|null => acc
+       case _ if(cur.head == ' ') => go(cur.tail, acc :+ '%' :+ '2' :+ '0') 
+       case _ => go(cur.tail, acc :+ cur.head)
     }
-
-    def ge(cur: Seq[Char], acc: Seq[Char]): Seq[Char] = {
-      println(s"cur:${cur} acc:${acc}")
-      cur.toList match {
-          case Nil =>   { println("Nil"); acc }
-          case head :: tail                  => { println(s"h:${head} t:${tail}"); go(tail, acc :+ head)}
-          case _ => error(_.toString)
-      }
-    }
-    val res = ge(urlSeq, Seq.empty[Char])
-    res.toString
+    go(url, "")
   }
-
-/*
-urlEncode :: String -> String
-urlEncode [] = []
-urlEncode (' ':xs) = "%20" ++ urlEncode xs
-urlEncode (x:xs) = x : urlEncode xs
-*/
 
 
   def main(args: Array[String]) {
-    println(urlEncode("aabcdsta") )
+    var res: String = urlEncode("aab cds ta")
+    println( urlEncode("aab cds ta") == "aab%20cds%20ta")
   }
 
 }
